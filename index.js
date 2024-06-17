5f698f0c84fa945911d5d3fda01b67ba2dbde319
matxon <kmpda@inbox.ru>
2024-06-17 11:47:59 +0500

    Жайлап тауарды қосып жатырмын

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/public', express.static('public'))
+app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
